f0f82a29b1883ecfb32cc367578384789a04fbdb
Carlos Eduardo <carloseduardo.and2@gmail.com>
2021-04-11 01:59:53 -0300

    feat: calculate function

--- scripts.js
+++ scripts.js
@@ -16,25 +16,48 @@ const Exchange = {
         .then(response => { response.json()    
             .then(data => {
                 const BRL_USD = data.BRL['bid'];
-                const BRL_EUR = data.EUR['bid'];
+                const EUR_BRL = data.EUR['bid'];
                 const USD_EUR = data.USD['bid'];
-                Exchange.calculate(BRL_USD, BRL_EUR, USD_EUR);
+                Exchange.calculate(BRL_USD, EUR_BRL, USD_EUR);
             })
         })
     },
 
-    calculate(BRL_USD, BRL_EUR, USD_EUR) {
+    calculate(BRL_USD, EUR_BRL, USD_EUR) {
         const slcCurrency1 = document.querySelector('#slcCurrency1').value;
         const slcCurrency2 = document.querySelector('#slcCurrency2').value;
         const exchangeValue = document.querySelector('#exchangeValue').value;
-        const result = 0;
+        let result = 0;
 
         if(exchangeValue === '' && slcCurrency1 === slcCurrency2) {
             alert('Valores inválidos. Tente novamente')
         } else {
-            if(slcCurrency1 === 'BRL' && slcCurrency1 === 'USD') {
-                result = BRL_USD*exchangeValue;
+            if(slcCurrency1 === 'BRL' && slcCurrency2 === 'USD') {
+                result = exchangeValue * BRL_USD;
+            } else if(slcCurrency1 === 'USD' && slcCurrency2 === 'BRL') {
+                result = exchangeValue / BRL_USD;
+            } else if(slcCurrency1 === 'BRL' && slcCurrency2 === 'EUR') {
+                result = exchangeValue * EUR_BRL;
+            } else if(slcCurrency1 === 'EUR' && slcCurrency2 === 'BRL') {
+                result = exchangeValue / EUR_BRL;
+            } else if(slcCurrency1 === 'EUR' && slcCurrency2 === 'USD') {
+                result = exchangeValue * USD_EUR;
+            } else if(slcCurrency1 === 'USD' && slcCurrency2 === 'EUR') {
+                result = exchangeValue / USD_EUR;
             }
+            DOM.innerHTMLResult(result.toFixed(2));
         }
+    }
+}
+
+const DOM = {
+    innerHTMLResult(result) {
+        const resultContainer = document.querySelector('.result-container');
+
+        const layout = `
+            <h2>${result}</h2>
+        `
+
+        resultContainer.innerHTML = layout;
     }
 }
